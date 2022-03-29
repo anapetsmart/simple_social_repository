453dd591a14fe3631882367d4d20e3258e2e75ab
Ana Peshku <apeshku1@terpmail.umd.edu>
2022-03-29 18:19:10 -0400

    created skeleton of connect method

---
+++ simple_social_network.py
@@ -0,0 +1,16 @@
+"""A simple social network for tracking connections between people. """
+
+class Person:
+    """A person in the social network.
+    Attributes:
+        name (str): the person's name. 
+        connections (set of Person): other people in the social network who
+            know this person."""
+    def __init__(self, name):
+        """Initialize a new Person object."""
+        self.name = name 
+        self.connections = set()
+    def connect(self, person2):
+        """Connect with person2.
+        Args:
+            person2 (Person): the other person to connect to."""
